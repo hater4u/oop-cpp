8c0083002f8a32ae0752f57a506fdff7a96ade4a
Nikita Drozdov <nik_drzdv@mail.ru>
2019-09-11 17:19:11 +0700

    start commiting

---
+++ lab0_intro_0b/lab0_intro_0b/lab0_intro_0b.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <map>
+
+int word_count(std::string inputf, std::string outputf) {
+	std::string line;
+
+	std::ifstream in(inputf);
+	std::ofstream out;
+	out.open(outputf);
+
+	/*if (in.is_open())
+	{
+		while (std::getline(in, line))
+		{
+			std::cout << line << std::endl;
+		}
+	}
+	in.close();*/
+
+	while (std::getline(in, line)) {
+
+		//std::cout << line << std::endl;
+	}
+
+	out.close();
+	in.close();
+	return 0;
+}
+
+int main(int argc, char** argv) {
+	word_count(argv[1], argv[2]);
+
+	return 0;
+}
